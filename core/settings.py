fc7bd8920c6d37a2ecce894a1843ee1405a78575
Nazim Husain <nazimhusain326@gmail.com>
2026-06-02 13:41:43 +0530

    settings

--- core/settings.py
+++ core/settings.py
@@ -177,11 +177,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True 
-CORS_ALLOWED_ORIGINS = [ 
-                        "https://netsight-frontend.vercel.app",
+CORS_ALLOWED_ORIGINS = [ "https://netsight-frontend.vercel.app",
                     ] 
+
+
 
 
 CORS_ALLOW_HEADERS = [
